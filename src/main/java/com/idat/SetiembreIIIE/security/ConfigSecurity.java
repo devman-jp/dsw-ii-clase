708efecfbd3ca332393fd77d677a685add177dc3
WilderEspinoza <wilder.espinozab@ciplima.org.pe>
2022-10-11 22:26:25 -0500

    Seguridad

--- src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
+++ src/main/java/com/idat/SetiembreIIIE/security/ConfigSecurity.java
@@ -3,9 +3,7 @@ package com.idat.SetiembreIIIE.security;
 import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,21 +19,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("profesor").password(encriptado().encode("123")).roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("estudiante").password(encriptado().encode("123")).roles("USER");
-
+		auth.inMemoryAuthentication().withUser("PROFESOR").password(encriptado().encode("123")).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("ALUMNO").password(encriptado().encode("123")).roles("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET).access("hasRole('USER')")
-			.antMatchers("/producto/v1/*").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/guardar").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/listar").permitAll()
 			.and()
 			.httpBasic()
 			.and()
